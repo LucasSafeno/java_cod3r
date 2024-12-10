01802fb921a3e8a96972f0552c03287d9db860d7
Lucas Safeno <lugnorio@gmail.com>
2024-12-10 11:13:26 -0300

    Udemy - Cod3r - Java 26.Tipos Primitivos#02

--- fundamentos/src/fundamentos/TiposPrimitivos.java
+++ fundamentos/src/fundamentos/TiposPrimitivos.java
@@ -2,7 +2,22 @@ package fundamentos;
 
 public class TiposPrimitivos {
 	public static void main(String[] args) {
+		// Informações do funcionário
 		
+		// Tipos númericos inteiro
+		byte anosDeEmpresa = 23;
+		short numeroDeVoos = 542;
+		int id = 56789;
+		long pontosAcumulados = 3_234_845_22;
+		
+		// Tipos numeros reais
+		float salario = 11_445.44F; // f ou F
+		double vendasAcumuladas = 2_991_797_103.01;
+		
+		// Tipo Booleano
+		boolean estarDeFerias = true; // ou false
+		
+		// Tipo caractere
+		char status = 'A'; // um caractere
 	}
-
-}
+}// TiposPrimitivos
