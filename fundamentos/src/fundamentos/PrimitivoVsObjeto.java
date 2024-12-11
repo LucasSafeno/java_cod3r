dd734076d60d53b7f84568d5936005e494b449b0
Lucas Safeno <lugnorio@gmail.com>
2024-12-11 10:45:54 -0300

    Udemy - Cod3r - Java - 31.Primtivos VS Objetos

---
+++ fundamentos/src/fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class PrimitivoVsObjeto {
+	public static void main(String[] args) {
+		String s = new String("texto");
+		
+		//wrappers são as versão objetos dos tipos primitivos
+		int a = 123;
+		System.out.println(a);
+	}
+}
